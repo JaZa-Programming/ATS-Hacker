0576db89b240f1c70fcbc5599ed3dc4726f80987
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-01 11:41:00 -0600

    Add tests for file processing

--- ats_hacker/api/process_file.py
+++ ats_hacker/api/process_file.py
@@ -14,6 +14,8 @@ def process_file(filename: str) -> str:
     # processing done for characters in aggregate(), it might make sense to
     # add the removal of newlines in that func instead of this one.
     #
-    # Also, make sure you close the file before this function returns so that
-    # we don't end up with any leaks.
-    pass
+    # Also, make sure you close the file (use the "with" keyword") before
+    # this function returns so that we don't end up with any leaks. If the file
+    # isn't found, you'll need to handle the IOError exception so that the
+    # program doesn't crash in an ugly way.
+    return
--- ats_hacker/tests/process_file_test.py
+++ ats_hacker/tests/process_file_test.py
@@ -1,3 +1,24 @@
 """Tests for the file_processor function."""
 
+import pytest
 from ats_hacker.api.process_file import process_file
+
+
+def test_sucessful_file_open():
+    filename = "test_data/simple-job.txt"
+    got = process_file(filename)
+    assert got is not None
+
+
+def test_unsuccessful_file_open():
+    filename = "test_data/doesnt-exist.txt"
+    with pytest.raises(IOError) as e:
+        assert process_file(filename)
+    assert str(e.value) == "File not found or inaccessable"
+
+
+def test_successful_file_processing():
+    filename = "test_data/simple-job.txt"
+    got = process_file(filename)
+    want = "Software Engineer\nSuper Cool Company\nBozeman, MT or Remote\n"
+    assert got == want
