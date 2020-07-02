7dfecc68457057d6b1baba392f937c4bbaed0523
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-02 08:38:32 -0600

    Update tests for new requirements and rename file for clarity

--- ats_hacker/tests/file_import_test.py
+++ ats_hacker/tests/file_import_test.py
@@ -5,20 +5,20 @@ from ats_hacker.api.import_file import import_file
 
 
 def test_sucessful_file_open():
-    filename = "test_data/simple-job.txt"
+    filename = "ats_hacker/tests/test_data/simple-job.txt"
     got = import_file(filename)
     assert got is not None
 
 
 def test_unsuccessful_file_open():
-    filename = "test_data/doesnt-exist.txt"
-    with pytest.raises(IOError) as e:
+    filename = "ats_hacker/tests/test_data/doesnt-exist.txt"
+    with pytest.raises(FileNotFoundError) as e:
         assert import_file(filename)
-    assert str(e.value) == "File not found or inaccessable"
+    assert str(e.value) == "No such file or directory: 'ats_hacker/tests/test_data/doesnt-exist.txt'"
 
 
 def test_successful_file_import():
-    filename = "test_data/simple-job.txt"
+    filename = "ats_hacker/tests/test_data/simple-job.txt"
     got = import_file(filename)
     want = "Software Engineer\nSuper Cool Company\nBozeman, MT or Remote\n"
     assert got == want
