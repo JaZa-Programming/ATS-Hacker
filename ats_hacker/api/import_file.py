248091528744feb8b65b1e2f03f8acdb27d99a3e
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-07-02 19:25:45 -0400

    Refactored code 3 failed / 6 pass

--- ats_hacker/api/import_file.py
+++ ats_hacker/api/import_file.py
@@ -1,7 +1,3 @@
-import os
-import errno
-
-
 """The file importer for ats_hacker.
 
 :param filename: A string representation of the filename to be processed.
@@ -13,15 +9,12 @@ import errno
 
 def import_file(filename: str) -> str:
     """Opens a file, processes it, and returns a string representation."""
-    file = ''
+    contents = ''
     try:
-        with open(filename, 'r') as f:
-           file = f.readlines()
-    except IOError as ioe:
-        if ioe.errno == errno.EACCES:
-            print('File not readable')
-        elif ioe.errno == errno.ENOENT:
-            print('File does not exist')
+        with open(filename, 'r') as file:
+           contents = file.readlines()
+    except FileNotFoundError as e:
+        raise FileNotFoundError('No such file or directory')
     
     # We will need to decide how to handle newline "\n" characters. Given the
     # processing done for characters in aggregate(), it might make sense to
@@ -31,4 +24,4 @@ def import_file(filename: str) -> str:
     # this function returns so that we don't end up with any leaks. If the file
     # isn't found, you'll need to handle the IOError exception so that the
     # program doesn't crash in an ugly way.
-    return file
+    return contents
