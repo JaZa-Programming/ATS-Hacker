78eac2410e4cb6df2a3e10c3e7bd05562f272dc3
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-03 16:17:10 -0600

    Add test for remove_words function

---
+++ ats_hacker/tests/remove_words_test.py
@@ -0,0 +1,21 @@
+"""Tests for the ATS-Hacker remove_words function."""
+
+from api.aggregator import aggregate
+from api.remove_words import remove_words
+import json
+
+
+def test_word_removal():
+    doc = "This is a test job description, a discription with a few words to remove."
+    keywords = aggregate(doc)
+    want = {
+        'test': 1,
+        'job': 1,
+        'description': 2,
+        'few': 1,
+        'words': 1,
+        'remove': 1
+    }
+    got = remove_words(json.loads(keywords),
+                       "ats_hacker/tests/test_data/words-to-remove.txt")
+    assert want == got
