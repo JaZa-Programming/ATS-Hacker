56e0530a6e1bc1025cc1d8aa175c4e4a6032003a
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-07-03 16:41:51 -0400

    import_file() returns string

--- ats_hacker/api/import_file.py
+++ ats_hacker/api/import_file.py
@@ -11,7 +11,7 @@ def import_file(filename: str) -> str:
     """Opens a file, processes it, and returns a string representation."""
     try:
         with open(filename, 'r') as file:
-            contents = file.readlines()
+            contents = file.read()
     except FileNotFoundError as e:
         raise FileNotFoundError(f'No such file or directory: {filename}')
     return contents
