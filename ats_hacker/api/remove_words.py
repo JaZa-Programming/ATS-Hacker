87bc137e120ade9c96450d670235ee47f265e0a9
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-03 15:59:53 -0600

    Update function signature and docstring for remove_words()

--- ats_hacker/api/remove_words.py
+++ ats_hacker/api/remove_words.py
@@ -1,11 +1,12 @@
 """The word removal function for ats_hacker.
 
 :param keywords: A dict made of keywords/counts.
+:param word_filename: A filename for txt file of words to remove.
 
 :returns: Mutated dict of keywords/counts.
 
 """
 
 
-def remove_words(doc: str) -> str:
+def remove_words(keywords: str, word_filename: str) -> str:
     pass
