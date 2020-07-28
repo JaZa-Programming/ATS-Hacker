aaa529d85de03928010bab204e7285672429ed6b
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-28 17:22:50 -0600

    Fix index out of range bug

--- ats_hacker/app.py
+++ ats_hacker/app.py
@@ -5,7 +5,7 @@ from web_server import create_app
 
 
 def run():
-    if sys.argv[1] == '-web':
+    if len(sys.argv) > 1 and sys.argv[1] == '-web':
         os.environ['FLASK_APP'] = 'ats_hacker.web_server:create_app()'
         create_app().run()
     else:
